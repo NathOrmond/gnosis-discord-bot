51e54b5f95c83a1ccaefe16be25d5e11543db372
nathan ormond <nath.m.ormond@gmail.com>
2021-02-19 19:23:10 +0000

    uncommit bad folders - init

---
+++ index.js
@@ -0,0 +1,7 @@
+// index.js
+const dotenv = require('dotenv');
+dotenv.config();
+// ... client setup (keep reading)
+console.log(process.env.A);
+console.log(process.env.B);
+client.login(process.env.TOKEN);
