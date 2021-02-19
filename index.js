fd8d04830777f51d2c7fba6cb0a7b1a4b8cac10c
nathan ormond <nath.m.ormond@gmail.com>
2021-02-19 20:04:41 +0000

    basic stuff

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // index.js
 const dotenv = require('dotenv');
 dotenv.config();
-// ... client setup (keep reading)
-console.log(process.env.A);
-console.log(process.env.B);
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
 client.login(process.env.TOKEN);
+
+client.on('message', message => {
+    console.log(message.content);
+    if (message.content === '!silly') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('No U!');
+    }
+});
