a6e3106c9e08801243d1a9d5f5d277a710011a6d
nathan ormond <nath.m.ormond@gmail.com>
2021-02-20 11:16:18 +0000

    message response map

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ dotenv.config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const keyWords = 
+[
+    "mathpig",
+    "silly",
+];
+
+const receivedResponseMap = 
+{
+    "mathpig" : "You Irish S.O.B",
+    "silly": "No U!"
+};
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
@@ -11,9 +23,48 @@ client.once('ready', () => {
 client.login(process.env.TOKEN);
 
 client.on('message', message => {
-    console.log(message.content);
-    if (message.content === '!silly') {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send('No U!');
+    console.log("Received: %s", message.content);
+    
+    if(isKeyWord(message.content)){ 
+        console.log("Keyword received master!");
+        // message.channel.send(getResponse(message.content));
     }
 });
+
+function isKeyWord(received){ 
+    return (keyWords.indexOf(received) >= 0);
+}
+
+function compare(strOne, strTwo){ 
+    return (strOne === strTwo);
+}
+
+function bruteSearch(ar, el){ 
+    ar.forEach(arEl => {
+        if(compare(el, arEl)){ 
+            return true;
+        }
+    });
+    return false;
+}
+
+function binarySearch(ar, el, compare_fn) {
+    var m = 0;
+    var n = ar.length - 1;
+    while (m <= n) {
+        var k = (n + m) >> 1;
+        var cmp = compare_fn(el, ar[k]);
+        if (cmp > 0) {
+            m = k + 1;
+        } else if(cmp < 0) {
+            n = k - 1;
+        } else {
+            return k;
+        }
+    }
+    return -m - 1;
+}
+
+function getResponse(received){ 
+    return receivedResponseMap.received;
+}
